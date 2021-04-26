7f05e0170ca80b718c46ae5ca5051f43543e12eb
chris-risk <61710960+chris-risk@users.noreply.github.com>
2021-04-26 01:00:10 -0400

    Update curltest.c

--- curltest.c
+++ curltest.c
@@ -3,24 +3,26 @@
 #include <string.h>
 #include <curl/curl.h>
 
+void printOutput(char *stringOutput, size_t size, size_t nmemb, void *stream)
+{    
+  printf("\n%s\n", stringOutput);
+}
+
 int main(void)
 {
   CURL *curl;
-  CURLcode res;
- 
+
   curl = curl_easy_init();
   if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://worldtimeapi.org/api/ip");
+    curl_easy_setopt(curl, CURLOPT_URL, "http://worldtimeapi.org/api/ip.txt");
     /* example.com is redirected, so we tell libcurl to follow redirection */ 
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, printOutput);
  
     /* Perform the request, res will get the return code */ 
-    res = curl_easy_perform(curl);
-    /* Check for errors */ 
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
  
+    curl_easy_perform(curl);
+    
     /* always cleanup */ 
     curl_easy_cleanup(curl);
   }
