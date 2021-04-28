21430b5e32f7ee256c613aac6f5d480588fdb5dd
Charles Carmichael <35904722+annorak@users.noreply.github.com>
2021-04-27 21:39:16 -0400

    Add files via upload

--- solved.c
+++ solved.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
  
 // N is the size of the 2D matrix   N*N
 #define N 9
@@ -62,7 +64,7 @@ to assign values to all unassigned locations in
 such a way to meet the requirements for
 Sudoku solution (non-duplication across rows,
 columns, and boxes) */
-int solveSuduko(int grid[N][N], int row, int col)
+int solvePuzzle(int grid[N][N], int row, int col)
 {
      
     // Check if we have reached the 8th row
@@ -86,7 +88,7 @@ int solveSuduko(int grid[N][N], int row, int col)
     // of the grid already contains
     // value >0, we iterate for next column
     if (grid[row][col] > 0)
-        return solveSuduko(grid, row, col + 1);
+        return solvePuzzle(grid, row, col + 1);
  
     for (int num = 1; num <= N; num++)
     {
@@ -106,7 +108,7 @@ int solveSuduko(int grid[N][N], int row, int col)
            
             //  Checking for next possibility with next
             //  column
-            if (solveSuduko(grid, row, col + 1)==1)
+            if (solvePuzzle(grid, row, col + 1)==1)
                 return 1;
         }
        
@@ -120,73 +122,104 @@ int solveSuduko(int grid[N][N], int row, int col)
     return 0;
 }
 
-void main()
+void printOutput(char *stringOutput, size_t size, size_t nmemb, void *stream)
 {
+    printf("API call follows: ")
+    printf("\n%s\n", stringOutput);
+}
+
+
+int main()
+{
+    
     int menuSelection = 0;
 
-    do{
-        spaceFiller();
-        printf("Welcome to the Sudoku Solver C Program!\n\n");
-        printf("Please select from the below menu what difficulty of puzzle you would like to solve: \n");
-        printf("1. Puzzle Difficulty: Easy\n");
-        printf("2. Puzzle Difficulty: Medium\n");
-        printf("3. Puzzle Difficulty: Hard\n");
-        spaceFiller();
+    CURL *curl;
 
-        scanf("%d", &menuSelection);
+    curl = curl_easy_init();
+    if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "http://worldclockapi.com/api/json/est/now");
+    /* example.com is redirected, so we tell libcurl to follow redirection */ 
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, printOutput);
+ 
+    /* Perform the request, res will get the return code */ 
+ 
+    curl_easy_perform(curl);
+    
+    /* always cleanup */ 
+    curl_easy_cleanup(curl);
 
-        printf("You chose option number: %d\n\n", menuSelection);
 
-        if(menuSelection == 1){
-            int gridEasy[N][N] =    
-                       {{ 3, 0, 6, 5, 0, 8, 4, 0, 0 },
-                       { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
-                       { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
-                       { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
-                       { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
-                       { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
-                       { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
-                       { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
-                       { 0, 0, 5, 2, 0, 6, 3, 0, 0 }} ;
-            if (solveSuduko(gridEasy, 0, 0)==1)
-                print(gridEasy);
-            else
-                printf("No solution exists");
-        }
 
-        else if(menuSelection == 2){
-                        int gridMedium[N][N] =    
-                        { { 2, 0, 0, 0, 0, 0, 1, 0, 0 },
-                        { 0, 8, 3, 0, 0, 0, 0, 0, 0 },
-                        { 1, 0, 0, 6, 8, 0, 4, 0, 0 },
-                        { 3, 0, 0, 0, 0, 6, 0, 0, 0 },
-                        { 0, 9, 8, 0, 7, 0, 3, 1, 0 },
-                        { 0, 0, 0, 9, 0, 0, 0, 0, 7 },
-                        { 0, 0, 2, 0, 9, 4, 0, 0, 1 },
-                        { 0, 0, 9, 0, 6, 0, 2, 5, 0 },
-                        { 0, 0, 6, 1, 0, 0, 0, 0, 9 } };
-            if (solveSuduko(gridMedium, 0, 0)==1)
-                print(gridMedium);
-            else
-                printf("No solution exists");
-        }
+    printf("Welcome to the Sudoku Solver C Program!\n\n");
 
-        else if(menuSelection == 3){
-                        int gridHard[N][N] =     
-                         { { 0, 0, 0, 0, 7, 0, 0, 0, 0 },
-                         { 0, 4, 0, 0, 0, 9, 0, 5, 0 },
-                         { 8, 0, 0, 6, 2, 0, 9, 0, 3 },
-                         { 6, 0, 0, 0, 3, 0, 2, 0, 0 },
-                         { 0, 0, 9, 0, 8, 0, 4, 0, 0 },
-                         { 0, 0, 5, 0, 5, 0, 0, 0, 6 },
-                         { 4, 0, 3, 0, 6, 7, 0, 0, 9 },
-                         { 0, 5, 0, 8, 0, 0, 0, 1, 0 },
-                         { 0, 0, 0, 0, 9, 0, 0, 0, 0 } };
-            if (solveSuduko(gridHard, 0, 0)==1)
-                print(gridHard);
-            else
-                printf("No solution exists");
-        }
-        }
-        while (0 == 0);
+
+    // do{
+    //     spaceFiller();
+    //     printf("Please select from the below menu what difficulty of puzzle you would like to solve: \n");
+    //     printf("1. Puzzle Difficulty: Easy\n");
+    //     printf("2. Puzzle Difficulty: Medium\n");
+    //     printf("3. Puzzle Difficulty: Hard\n");
+    //     spaceFiller();
+
+    //     scanf("%d", &menuSelection);
+
+    //     printf("You chose option number: %d\n\n", menuSelection);
+
+    //     if(menuSelection == 1){
+    //         int gridEasy[N][N] =    
+    //                    {{ 3, 0, 6, 5, 0, 8, 4, 0, 0 },
+    //                    { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
+    //                    { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
+    //                    { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
+    //                    { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
+    //                    { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
+    //                    { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
+    //                    { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
+    //                    { 0, 0, 5, 2, 0, 6, 3, 0, 0 }} ;
+    //         if (solvePuzzle(gridEasy, 0, 0)==1)
+    //             print(gridEasy);
+    //         else
+    //             printf("No solution exists\n");
+    //     }
+
+    //     else if(menuSelection == 2){
+    //                     int gridMedium[N][N] =    
+    //                     { { 2, 0, 0, 0, 0, 0, 1, 0, 0 },
+    //                     { 0, 8, 3, 0, 0, 0, 0, 0, 0 },
+    //                     { 1, 0, 0, 6, 8, 0, 4, 0, 0 },
+    //                     { 3, 0, 0, 0, 0, 6, 0, 0, 0 },
+    //                     { 0, 9, 8, 0, 7, 0, 3, 1, 0 },
+    //                     { 0, 0, 0, 9, 0, 0, 0, 0, 7 },
+    //                     { 0, 0, 2, 0, 9, 4, 0, 0, 1 },
+    //                     { 0, 0, 9, 0, 6, 0, 2, 5, 0 },
+    //                     { 0, 0, 6, 1, 0, 0, 0, 0, 9 } };
+    //         if (solvePuzzle(gridMedium, 0, 0)==1)
+    //             print(gridMedium);
+    //         else
+    //             printf("No solution exists\n");
+    //     }
+
+    //     else if(menuSelection == 3){
+    //                     int gridHard[N][N] =     
+    //                      { { 0, 0, 0, 0, 7, 0, 0, 0, 0 },
+    //                      { 0, 4, 0, 0, 0, 9, 0, 5, 0 },
+    //                      { 8, 0, 0, 6, 2, 0, 9, 0, 3 },
+    //                      { 6, 0, 0, 0, 3, 0, 2, 0, 0 },
+    //                      { 0, 0, 9, 0, 8, 0, 4, 0, 0 },
+    //                      { 0, 0, 5, 0, 5, 0, 0, 0, 6 },
+    //                      { 4, 0, 3, 0, 6, 7, 0, 0, 9 },
+    //                      { 0, 5, 0, 8, 0, 0, 0, 1, 0 },
+    //                      { 0, 0, 0, 0, 9, 0, 0, 0, 0 } };
+    //         if (solvePuzzle(gridHard, 0, 0)==1)
+    //             print(gridHard);
+    //         else
+    //             printf("No solution exists\n");
+    //     }
+    //     }
+    //     while (0 == 0);
+
+    }
+    return 0;
 }
